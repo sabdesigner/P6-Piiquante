923597db9bb0e5542e234fc4addafa9eaa021fe9
Sabrina Ambre Biller <contact@sabrinambre.fr>
2022-09-12 01:55:59 +0200

    Integration de MongoDB Étape 1 + Middelware autorisations

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,60 @@
+// Import d'Express
 const express = require('express');
 
+// Utilisation d'Express
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+// Import de mongoDB
+const mongoose = require('mongoose');
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+// Configuration de mongoDB
+mongoose.connect('mongodb+srv://SabDesigner:SabDesigner@p6.fskvzsg.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+//Intégration des Middlewares
+//méthode app.use() permet d'attribuer un middleware à une route spécifique
+// Intégration du body
+app.use(express.json());
 
+// Ajout des Middlewares d'autorisations
 app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  /* //exemple cours OC
+  app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
+
+  app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });*/
 
 module.exports = app;
